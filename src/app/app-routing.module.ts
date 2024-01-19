17010715c25b5d1d636b95e57795cbf8e98b99f2
Omar <laomy50@gmail.com>
2024-01-19 09:58:04 +0300

    omar commited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {path:'',component:LinkerComponent,
   children:[
     {path:'',component:LandingComponent},
-    {}
+
   ]
 }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import {MatInputModule} from '@angular/material/input';
     NavComponent,
     LinkerComponent,
     LandingComponent,
-    LoginComponent,
+    LoginComponent
    
   ],
   imports: [
