f6925bac52d58287882a442d7b111906d456f7a4
Omar <laomy50@gmail.com>
2024-05-17 14:00:36 +0300

    commited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,7 +44,7 @@ children:[
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,  { useHash: true, bindToComponentInputs: true, initialNavigation: 'enabledBlocking' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
