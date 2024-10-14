9a53399216afa411f85b7d196f0aa66686bf944b
Omar <laomy50@gmail.com>
2024-10-14 10:23:12 +0300

    staff pages

--- src/app/staffcomponent/allorders/allorders.component.ts
+++ src/app/staffcomponent/allorders/allorders.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-allorders',
   templateUrl: './allorders.component.html',
   styleUrl: './allorders.component.css'
 })
-export class AllordersComponent {
+export class AllordersComponent implements OnInit{
+
+
+  
+  displayedColumns: string[] = ['id', 'name', 'phone', 'email','status','action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+payment: any;
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  open(payment: any){}
 
 }
+
+
+
